de28b50029c5308284e42712d7a0478c88262cec
Mihail-K <mintiewute@gmail.com>
2020-06-15 21:47:40 -0400

    Add EBO to sprite renderer and reduce vertices.

--- source/game_engine/core/sprite_renderer.d
+++ source/game_engine/core/sprite_renderer.d
@@ -20,13 +20,15 @@ struct SpriteConfig
 
 immutable float[] vertices = [
     // pos      // tex
-    0.0f, 1.0f, 0.0f, 1.0f,
-    1.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 0.0f, 
+    1.0f, 1.0f, 1.0f, 1.0f, // top right
+    0.0f, 1.0f, 0.0f, 1.0f, // bottom right
+    0.0f, 0.0f, 0.0f, 0.0f, // bottom left
+    1.0f, 0.0f, 1.0f, 0.0f  // top left
+];
 
-    0.0f, 1.0f, 0.0f, 1.0f,
-    1.0f, 1.0f, 1.0f, 1.0f,
-    1.0f, 0.0f, 1.0f, 0.0f
+immutable uint[] indices = [
+    0, 1, 3, // 1st triangle
+    1, 2, 3  // 2nd triangle
 ];
 
 struct SpriteRenderer
@@ -70,21 +72,26 @@ public:
         config.texture.bind();
 
         glBindVertexArray(_quadVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, cast(void*) 0);
         glBindVertexArray(0);
     }
 
 private:
     void initRenderData()
     {
-        uint VBO;
+        uint VBO, EBO;
 
         glGenVertexArrays(1, &_quadVAO);
         glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
         glBindVertexArray(_quadVAO);
 
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, vertices.length * float.sizeof, vertices.ptr, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.length * uint.sizeof, indices.ptr, GL_STATIC_DRAW);
 
         glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * float.sizeof, cast(void*) 0);
         glEnableVertexAttribArray(0);
